3e3592e46eacce54765d891df597f5fcf454609d
Leo <jafee201153@gmail.com>
2025-10-31 15:56:03 +0800

    feat(minigrep): add case-insensitive search and update README with examples

--- minigrep/README.md
+++ minigrep/README.md
@@ -11,5 +11,38 @@ cargo run -- <query> <file_path>
 You can try it out with the following command:
 
 ```bash
+cargo run -- body poem.txt
+#    Compiling minigrep v0.1.0 (/Users/leoli/Desktop/SideProject/rust/minigrep)
+#     Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.14s
+#      Running `target/debug/minigrep body poem.txt`
+# Searching for 'body'
+# In file 'poem.txt'
+# I'm nobody! Who are you?
+# Are you nobody, too?
+# How dreary to be somebody!
+cargo run -- monomorphization poem.txt
+#     Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.00s
+#      Running `target/debug/minigrep monomorphization poem.txt`
+# Searching for 'monomorphization'
+# In file 'poem.txt'
+```
+
+```bash
 cargo run -- to poem.txt
+#     Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.00s
+#      Running `target/debug/minigrep to poem.txt`
+# Searching for 'to'
+# In file 'poem.txt'
+# Are you nobody, too?
+# How dreary to be somebody!
+IGNORE_CASE=1 cargo run -- to poem.txt
+#    Compiling minigrep v0.1.0 (/Users/leoli/Desktop/SideProject/rust/minigrep)
+#     Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.12s
+#      Running `target/debug/minigrep to poem.txt`
+# Searching for 'to'
+# In file 'poem.txt'
+# Are you nobody, too?
+# How dreary to be somebody!
+# To tell your name the livelong day
+# To an admiring bog!
 ```
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,7 +4,7 @@
 // use std::process;
 use std::{env, error::Error, fs, process};
 
-use minigrep::search;
+use minigrep::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,18 +14,19 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for \'{}\'", config.query);
-    println!("In file \'{}\'", config.file_path);
+    // println!("Searching for \'{}\'", config.query);
+    // println!("In file \'{}\'", config.file_path);
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
 
-struct Config {
-    query: String,
-    file_path: String,
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -38,14 +39,26 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok(); // Check if the environment variable is set
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
